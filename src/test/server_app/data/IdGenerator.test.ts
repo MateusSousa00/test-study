032b6730dfec23f14c6530c6b53b200dcbb040a8
Mateus Lima <mateus.sousa00@gmail.com>
2023-06-12 15:33:17 -0300

    feat: created tests with high mock to server app

---
+++ src/test/server_app/data/IdGenerator.test.ts
@@ -0,0 +1,8 @@
+import { generateRandomId } from "../../../app/server_app/data/IdGenerator";
+
+describe("IdGenerator test suite", () => {
+  it("should return a random string", () => {
+    const randomId = generateRandomId();
+    expect(randomId.length).toBe(20);
+  });
+});
