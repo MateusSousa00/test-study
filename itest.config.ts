76d11e7b678e3a2a5e3b0b4dbb9e1ff8ba94912f
Mateus Lima <mateus.lsousa00@gmail.com>
2023-06-12 23:00:24 -0300

    feat: created workflow

--- itest.config.ts
+++ itest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const baseDir = "<rootDir>/src/app/server_app";
-const baseTestDir = "<rootDir>/src/test/";
+const baseTestDir = "<rootDir>/src/test/server_app3";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
@@ -9,10 +9,7 @@ const config: Config.InitialOptions = {
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: [`${baseDir}/**/*.ts`],
-  testMatch: [
-    `${baseTestDir}/server_app/**/*test.ts`,
-    `${baseTestDir}/server_app2/**/*test.ts`,
-  ],
+  testMatch: [`${baseTestDir}/**/*test.ts`],
   setupFiles: ["<rootDir>/src/test/server_app3/utils/config.ts"],
 };
 
